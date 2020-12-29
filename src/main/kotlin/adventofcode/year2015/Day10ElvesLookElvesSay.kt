522c5fc3137f27dc9600bfcc4d700f88a7487b7e
Peter Folta <mail@peterfolta.net>
2020-12-29 00:15:48 +0000

    [2015/10] Elves Look, Elves Say

---
+++ src/main/kotlin/adventofcode/year2015/Day10ElvesLookElvesSay.kt
@@ -0,0 +1,27 @@
+package adventofcode.year2015
+
+import adventofcode.Puzzle
+
+class Day10ElvesLookElvesSay(customInput: String? = null) : Puzzle(customInput) {
+    override val name = "Elves Look, Elves Say"
+
+    override fun partOne() = generateSequence(input) { it.elfLookElfSay() }.drop(1).take(40).last().length
+
+    override fun partTwo() = generateSequence(input) { it.elfLookElfSay() }.drop(1).take(50).last().length
+
+    companion object {
+        private fun String.elfLookElfSay(): String {
+            val digitGroups = mutableListOf<Pair<Int, Int>>()
+
+            map { it.toString().toInt() }.forEach { digit ->
+                if (digitGroups.isNotEmpty() && digitGroups.last().second == digit) {
+                    digitGroups[digitGroups.size - 1] = digitGroups.last().first + 1 to digit
+                } else {
+                    digitGroups.add(1 to digit)
+                }
+            }
+
+            return digitGroups.flatMap { it.toList() }.joinToString("")
+        }
+    }
+}
