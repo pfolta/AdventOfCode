f564e39af0fd0dec8832b5ef37c62f234a64085a
Peter Folta <mail@peterfolta.net>
2020-12-31 00:34:14 +0000

    [2017/4] High-Entropy Passphrases (Part 2)

--- src/main/kotlin/adventofcode/year2017/Day04HighEntropyPassphrases.kt
+++ src/main/kotlin/adventofcode/year2017/Day04HighEntropyPassphrases.kt
@@ -10,4 +10,10 @@ class Day04HighEntropyPassphrases(customInput: String? = null) : Puzzle(customIn
         .map { passphrase -> passphrase.split(" ").groupingBy { it }.eachCount().values }
         .filter { passphrase -> passphrase.none { it > 1 } }
         .count()
+
+    override fun partTwo() = input
+        .lines()
+        .map { passphrase -> passphrase.split(" ").map { password -> password.groupingBy { it }.eachCount() } }
+        .filter { passphrase -> passphrase == passphrase.distinct() }
+        .count()
 }
