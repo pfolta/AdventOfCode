e1732b51062687e70b0d5d09196dabd995148452
Peter Folta <mail@peterfolta.net>
2020-12-31 01:08:08 +0000

    [2018/1] Chronal Calibration (Part 2)

--- src/main/kotlin/adventofcode/year2018/Day01ChronalCalibration.kt
+++ src/main/kotlin/adventofcode/year2018/Day01ChronalCalibration.kt
@@ -3,5 +3,19 @@ package adventofcode.year2018
 import adventofcode.Puzzle
 
 class Day01ChronalCalibration(customInput: String? = null) : Puzzle(customInput) {
-    override fun partOne() = input.lines().map(String::toInt).sum()
+    private val frequencies = input.lines().map(String::toInt)
+
+    override fun partOne() = frequencies.sum()
+
+    override fun partTwo(): Int {
+        val seen = hashSetOf<Int>()
+
+        return generateSequence(-1 to 0) { previous ->
+            seen.add(previous.second)
+            val index = (previous.first + 1) % frequencies.size
+            index to previous.second + frequencies[index]
+        }
+            .map { it.second }
+            .first(seen::contains)
+    }
 }
--- src/test/kotlin/adventofcode/year2018/Day01ChronalCalibrationSpec.kt
+++ src/test/kotlin/adventofcode/year2018/Day01ChronalCalibrationSpec.kt
@@ -25,5 +25,38 @@ class Day01ChronalCalibrationSpec : PuzzleBaseSpec(
             -2
             -3
         """.trimIndent() to -6
+    ),
+    listOf(
+        """
+            +1
+            -2
+            +3
+            +1
+        """.trimIndent() to 2,
+        """
+            +1
+            -1
+        """.trimIndent() to 0,
+        """
+            +3
+            +3
+            +4
+            -2
+            -4
+        """.trimIndent() to 10,
+        """
+            -6
+            +3
+            +8
+            +5
+            -6
+        """.trimIndent() to 5,
+        """
+            +7
+            +7
+            -2
+            -7
+            -4
+        """.trimIndent() to 14
     )
 )
