04de3027cf8c84a8cfecb9ca1809d6a777e47f9b
Peter Folta <mail@peterfolta.net>
2021-01-01 11:42:53 +0000

    [2018/3] No Matter How You Slice It (Part 2)

--- src/main/kotlin/adventofcode/year2018/Day03NoMatterHowYouSliceIt.kt
+++ src/main/kotlin/adventofcode/year2018/Day03NoMatterHowYouSliceIt.kt
@@ -6,16 +6,20 @@ import adventofcode.common.product
 import adventofcode.common.removeAt
 
 class Day03NoMatterHowYouSliceIt(customInput: String? = null) : Puzzle(customInput) {
-    override fun partOne() = input
-        .lines()
-        .map {
-            val (id, left, top, width, height) = INPUT_REGEX.find(it)!!.destructured
-            Claim(id.toInt(), left.toInt(), top.toInt(), width.toInt(), height.toInt())
-        }
-        .flatMap(Claim::area)
-        .groupingBy { it }
-        .eachCount()
-        .count { it.value > 1 }
+    private val claims by lazy {
+        input
+            .lines()
+            .map {
+                val (id, left, top, width, height) = INPUT_REGEX.find(it)!!.destructured
+                Claim(id.toInt(), left.toInt(), top.toInt(), width.toInt(), height.toInt())
+            }
+    }
+
+    private val fabric by lazy { claims.flatMap(Claim::area).groupingBy { it }.eachCount() }
+
+    override fun partOne() = fabric.count { it.value > 1 }
+
+    override fun partTwo() = claims.first { claim -> claim.area.all { fabric[it] == 1 } }.id
 
     companion object {
         private val INPUT_REGEX = """#(\d+) @ (\d+),(\d+): (\d+)x(\d+)""".toRegex()
--- src/test/kotlin/adventofcode/year2018/Day03NoMatterHowYouSliceItSpec.kt
+++ src/test/kotlin/adventofcode/year2018/Day03NoMatterHowYouSliceItSpec.kt
@@ -2,4 +2,4 @@ package adventofcode.year2018
 
 import adventofcode.PuzzleBaseSpec
 
-class Day03NoMatterHowYouSliceItSpec : PuzzleBaseSpec(4)
+class Day03NoMatterHowYouSliceItSpec : PuzzleBaseSpec(4, 3)
