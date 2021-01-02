9c3ae84db5b58ff35fe674f8791f86c703c58b2b
Peter Folta <mail@peterfolta.net>
2021-01-02 19:32:25 +0000

    [2015/6] Probably a Fire Hazard (Part 2)

--- src/main/kotlin/adventofcode/year2015/Day06ProbablyAFireHazard.kt
+++ src/main/kotlin/adventofcode/year2015/Day06ProbablyAFireHazard.kt
@@ -5,6 +5,7 @@ import adventofcode.common.cartesianProduct
 import adventofcode.year2015.Day06ProbablyAFireHazard.Companion.Action.TOGGLE
 import adventofcode.year2015.Day06ProbablyAFireHazard.Companion.Action.TURN_OFF
 import adventofcode.year2015.Day06ProbablyAFireHazard.Companion.Action.TURN_ON
+import kotlin.math.max
 
 class Day06ProbablyAFireHazard(customInput: String? = null) : Puzzle(customInput) {
     override val name = "Probably a Fire Hazard"
@@ -27,6 +28,16 @@ class Day06ProbablyAFireHazard(customInput: String? = null) : Puzzle(customInput
 
         lights
     }.count { (_, state) -> state }
+
+    override fun partTwo() = instructions.fold(mutableMapOf<Pair<Int, Int>, Int>()) { lights, instruction ->
+        when (instruction.action) {
+            TURN_ON -> instruction.lights.forEach { light -> lights[light] = lights.getOrDefault(light, 0) + 1 }
+            TURN_OFF -> instruction.lights.forEach { light -> lights[light] = max(0, lights.getOrDefault(light, 0) - 1) }
+            TOGGLE -> instruction.lights.forEach { light -> lights[light] = lights.getOrDefault(light, 0) + 2 }
+        }
+
+        lights
+    }.values.sum()
 
     companion object {
         private val INPUT_REGEX = """(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)""".toRegex()
