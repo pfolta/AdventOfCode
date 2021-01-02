3e015b22cbd8ac507b29626da8e793b6215bcf37
Peter Folta <mail@peterfolta.net>
2021-01-02 19:22:02 +0000

    [2015/6] Probably a Fire Hazard (Part 1)

---
+++ src/main/kotlin/adventofcode/year2015/Day06ProbablyAFireHazard.kt
@@ -0,0 +1,54 @@
+package adventofcode.year2015
+
+import adventofcode.Puzzle
+import adventofcode.common.cartesianProduct
+import adventofcode.year2015.Day06ProbablyAFireHazard.Companion.Action.TOGGLE
+import adventofcode.year2015.Day06ProbablyAFireHazard.Companion.Action.TURN_OFF
+import adventofcode.year2015.Day06ProbablyAFireHazard.Companion.Action.TURN_ON
+
+class Day06ProbablyAFireHazard(customInput: String? = null) : Puzzle(customInput) {
+    override val name = "Probably a Fire Hazard"
+
+    private val instructions by lazy {
+        input
+            .lines()
+            .map {
+                val (action, left, top, right, bottom) = INPUT_REGEX.find(it)!!.destructured
+                Instruction(Action.fromString(action)!!, left.toInt(), top.toInt(), right.toInt(), bottom.toInt())
+            }
+    }
+
+    override fun partOne() = instructions.fold(mutableMapOf<Pair<Int, Int>, Boolean>()) { lights, instruction ->
+        when (instruction.action) {
+            TURN_ON -> instruction.lights.forEach { light -> lights[light] = true }
+            TURN_OFF -> instruction.lights.forEach { light -> lights[light] = false }
+            TOGGLE -> instruction.lights.forEach { light -> lights[light] = !lights.getOrDefault(light, false) }
+        }
+
+        lights
+    }.count { (_, state) -> state }
+
+    companion object {
+        private val INPUT_REGEX = """(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)""".toRegex()
+
+        data class Instruction(
+            val action: Action,
+            val left: Int,
+            val top: Int,
+            val right: Int,
+            val bottom: Int
+        ) {
+            val lights by lazy { listOf((left..right).toList(), (top..bottom).toList()).cartesianProduct().map { it.first() to it.last() } }
+        }
+
+        enum class Action(val action: String) {
+            TURN_ON("turn on"),
+            TURN_OFF("turn off"),
+            TOGGLE("toggle");
+
+            companion object {
+                fun fromString(action: String) = values().associateBy(Action::action)[action]
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/adventofcode/year2015/Day06ProbablyAFireHazardSpec.kt
@@ -0,0 +1,17 @@
+package adventofcode.year2015
+
+import adventofcode.PuzzleBaseSpec
+
+class Day06ProbablyAFireHazardSpec : PuzzleBaseSpec(
+    listOf(
+        "turn on 0,0 through 999,999" to 1_000_000,
+        """
+            turn on 0,0 through 999,999
+            toggle 0,0 through 999,0
+        """.trimIndent() to 999_000,
+        """
+            turn on 0,0 through 999,999
+            turn off 499,499 through 500,500
+        """.trimIndent() to 999_996
+    )
+)
