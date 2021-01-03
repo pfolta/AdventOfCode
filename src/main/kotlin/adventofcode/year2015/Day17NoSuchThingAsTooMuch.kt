47c5e86f9b47fc7d719018335a07cbec30c5fe4b
Peter Folta <mail@peterfolta.net>
2021-01-03 00:38:14 +0000

    [2015/17] No Such Thing as Too Much (Part 2)

--- src/main/kotlin/adventofcode/year2015/Day17NoSuchThingAsTooMuch.kt
+++ src/main/kotlin/adventofcode/year2015/Day17NoSuchThingAsTooMuch.kt
@@ -6,5 +6,9 @@ import adventofcode.common.powersets
 class Day17NoSuchThingAsTooMuch(customInput: String? = null) : Puzzle(customInput) {
     override val name = "No Such Thing as Too Much"
 
-    override fun partOne() = input.lines().map(String::toInt).powersets().filter { it.sum() == 150 }.size
+    private val combinations by lazy { input.lines().map(String::toInt).powersets().filter { it.sum() == 150 } }
+
+    override fun partOne() = combinations.size
+
+    override fun partTwo() = combinations.filter { it.size == combinations.minByOrNull { it.size }!!.size }.size
 }
--- src/test/kotlin/adventofcode/year2015/Day17NoSuchThingAsTooMuchSpec.kt
+++ src/test/kotlin/adventofcode/year2015/Day17NoSuchThingAsTooMuchSpec.kt
@@ -2,4 +2,4 @@ package adventofcode.year2015
 
 import adventofcode.PuzzleBaseSpec
 
-class Day17NoSuchThingAsTooMuchSpec : PuzzleBaseSpec(4)
+class Day17NoSuchThingAsTooMuchSpec : PuzzleBaseSpec(4, 3)
