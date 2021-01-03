8936bcb001c287c2679abf2061ea6b7ab98581e6
Peter Folta <mail@peterfolta.net>
2021-01-03 01:26:42 +0000

    [2015/18] Like a GIF For Your Yard (Part 2)

--- src/main/kotlin/adventofcode/year2015/Day18LikeAGifForYourYard.kt
+++ src/main/kotlin/adventofcode/year2015/Day18LikeAGifForYourYard.kt
@@ -6,30 +6,24 @@ import adventofcode.common.cartesianProduct
 class Day18LikeAGifForYourYard(customInput: String? = null) : Puzzle(customInput) {
     override val name = "Like a GIF For Your Yard"
 
-    override fun partOne(): Int {
-        val initialConfiguration = input
+    private val initialConfiguration by lazy {
+        input
             .lines()
-            .flatMapIndexed { y, row -> row.mapIndexed { x, light -> Pair(x, y) to (light == TURNED_ON) } }
+            .flatMapIndexed { y, row -> row.mapIndexed { x, state -> Pair(x, y) to (state == TURNED_ON) } }
             .toMap()
-
-        return generateSequence(initialConfiguration) { previous ->
-            previous + previous.mapNotNull { (light, state) ->
-                val turnedOnNeighbors = light.neighbors().count { previous[it]!! }
-
-                if (state && listOf(2, 3).none { turnedOnNeighbors == it }) {
-                    light to false
-                } else if (!state && turnedOnNeighbors == 3) {
-                    light to true
-                } else {
-                    null
-                }
-            }
-        }
-            .drop(1)
-            .take(100)
-            .last()
-            .count { (_, state) -> state }
     }
+
+    override fun partOne() = initialConfiguration.animate()
+        .drop(1)
+        .take(STEP_COUNT)
+        .last()
+        .count { (_, turnedOn) -> turnedOn }
+
+    override fun partTwo() = initialConfiguration.animate(setOf(Pair(0, 0), Pair(99, 0), Pair(0, 99), Pair(99, 99)))
+        .drop(1)
+        .take(STEP_COUNT)
+        .last()
+        .count { (_, turnedOn) -> turnedOn }
 
     companion object {
         private const val TURNED_ON = '#'
@@ -37,7 +31,23 @@ class Day18LikeAGifForYourYard(customInput: String? = null) : Puzzle(customInput
         private const val GRID_HEIGHT = 100
         private const val GRID_WIDTH = 100
 
-        fun Pair<Int, Int>.neighbors() = listOf((-1..1).toList(), (-1..1).toList())
+        private const val STEP_COUNT = 100
+
+        fun Map<Pair<Int, Int>, Boolean>.animate(stuckLights: Set<Pair<Int, Int>> = emptySet()) = generateSequence(this) { previous ->
+            previous + previous.minus(stuckLights).mapNotNull { (light, turnedOn) ->
+                val turnedOnNeighbors = light.neighbors().count { previous[it]!! }
+
+                if (turnedOn && listOf(2, 3).none { turnedOnNeighbors == it }) {
+                    light to false
+                } else if (!turnedOn && turnedOnNeighbors == 3) {
+                    light to true
+                } else {
+                    null
+                }
+            }
+        }
+
+        private fun Pair<Int, Int>.neighbors() = listOf((-1..1).toList(), (-1..1).toList())
             .cartesianProduct()
             .map { this + (it.first() to it.last()) }
             .minus(this)
