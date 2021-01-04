a1aef54849ae08814458d3bd822eb97e63a8694e
Peter Folta <mail@peterfolta.net>
2021-01-04 00:51:02 +0000

    [2015/20] Infinite Elves and Infinite Houses (Part 2)

--- src/main/kotlin/adventofcode/year2015/Day20InfiniteElvesAndInfiniteHouses.kt
+++ src/main/kotlin/adventofcode/year2015/Day20InfiniteElvesAndInfiniteHouses.kt
@@ -9,4 +9,8 @@ class Day20InfiniteElvesAndInfiniteHouses(customInput: String? = null) : Puzzle(
     private val presents by lazy { input.toInt() }
 
     override fun partOne() = generateSequence(1, Int::inc).first { house -> house.divisors().map { it * 10 }.sum() >= presents }
+
+    override fun partTwo() = generateSequence(1, Int::inc)
+        .first { house -> house.divisors().filter { divisor -> house / divisor <= 50 }.map { it * 11 }.sum() >= presents }
+
 }
