749ddb160bbd2488988cd7106a97db5e59625bfd
Peter Folta <mail@peterfolta.net>
2021-12-01 10:45:52 +0000

    [2021/1] Sonar Sweep - use `windowed`

--- src/main/kotlin/adventofcode/year2021/Day01SonarSweep.kt
+++ src/main/kotlin/adventofcode/year2021/Day01SonarSweep.kt
@@ -5,10 +5,7 @@ import adventofcode.Puzzle
 class Day01SonarSweep(customInput: String? = null) : Puzzle(customInput) {
     private val depths by lazy { input.lines().map(String::toInt) }
 
-    override fun partOne() = depths.zipWithNext().count { it.second > it.first }
+    override fun partOne() = depths.windowed(2).count { it.last() > it.first() }
 
-    override fun partTwo() = (0 until depths.size - 2)
-        .map { start -> depths[start] + depths[start + 1] + depths[start + 2] }
-        .zipWithNext()
-        .count { it.second > it.first }
+    override fun partTwo() = depths.windowed(3).map(List<Int>::sum).windowed(2).count { it.last() > it.first() }
 }
