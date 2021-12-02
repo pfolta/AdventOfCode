23ddc7f26a27a503c1e37e2520a9bc0d65858ed0
Peter Folta <mail@peterfolta.net>
2021-12-02 06:04:13 +0000

    [2021/2] Dive! (Part 1)

---
+++ src/main/kotlin/adventofcode/year2021/Day02Dive.kt
@@ -0,0 +1,46 @@
+package adventofcode.year2021
+
+import adventofcode.Puzzle
+import adventofcode.year2021.Day02Dive.Companion.Direction.DOWN
+import adventofcode.year2021.Day02Dive.Companion.Direction.FORWARD
+import adventofcode.year2021.Day02Dive.Companion.Direction.UP
+
+class Day02Dive(customInput: String? = null) : Puzzle(customInput) {
+    override val name = "Dive!"
+
+    private val commands by lazy { input.lines().map(::Command) }
+
+    override fun partOne() = commands.fold(Position()) { acc, command ->
+        when (command.direction) {
+            FORWARD -> Position(acc.horizontal + command.value, acc.depth)
+            DOWN -> Position(acc.horizontal, acc.depth + command.value)
+            UP -> Position(acc.horizontal, acc.depth - command.value)
+        }
+    }.multiply()
+
+    companion object {
+        data class Position(
+            val horizontal: Int = 0,
+            val depth: Int = 0
+        ) {
+            fun multiply() = horizontal * depth
+        }
+
+        data class Command(
+            val direction: Direction,
+            val value: Int
+        ) {
+            constructor(input: String) : this(Direction.fromString(input.split(" ").first())!!, input.split(" ").last().toInt())
+        }
+
+        enum class Direction(val direction: String) {
+            FORWARD("forward"),
+            DOWN("down"),
+            UP("up");
+
+            companion object {
+                fun fromString(direction: String) = values().associateBy(Direction::direction)[direction]
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/adventofcode/year2021/Day02DiveSpec.kt
@@ -0,0 +1,5 @@
+package adventofcode.year2021
+
+import adventofcode.PuzzleBaseSpec
+
+class Day02DiveSpec : PuzzleBaseSpec(150)
