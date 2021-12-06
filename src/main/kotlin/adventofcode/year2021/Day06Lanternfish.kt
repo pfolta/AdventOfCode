c06eb0db11f4a493b41b0bfcfa9a1a74680b0535
Peter Folta <mail@peterfolta.net>
2021-12-06 08:33:12 +0000

    [2021/6] Lanternfish (Part 2)

--- src/main/kotlin/adventofcode/year2021/Day06Lanternfish.kt
+++ src/main/kotlin/adventofcode/year2021/Day06Lanternfish.kt
@@ -5,10 +5,24 @@ import adventofcode.Puzzle
 class Day06Lanternfish(customInput: String? = null) : Puzzle(customInput) {
     private val fish by lazy { input.split(",").map(String::toInt) }
 
+    private val fishByAge by lazy { fish.groupingBy { it }.eachCount().map { (age, count) -> age to count.toLong() }.toMap() }
+
     override fun partOne() = generateSequence(fish) { previous ->
         previous.flatMap { if (it == 0) listOf(6, 8) else listOf(it - 1) }
     }
         .take(80 + 1)
         .last()
         .size
+
+    override fun partTwo() = generateSequence(fishByAge) { previous ->
+        previous
+            .flatMap { (age, count) -> if (age == 0) listOf(6 to count, 8 to count) else listOf(age - 1 to count) }
+            .groupBy { (age, _) -> age }
+            .map { (age, count) -> age to count.sumOf { it.second } }
+            .toMap()
+    }
+        .take(256 + 1)
+        .last()
+        .values
+        .sum()
 }
--- src/test/kotlin/adventofcode/year2021/Day06LanternfishSpec.kt
+++ src/test/kotlin/adventofcode/year2021/Day06LanternfishSpec.kt
@@ -2,4 +2,4 @@ package adventofcode.year2021
 
 import adventofcode.PuzzleBaseSpec
 
-class Day06LanternfishSpec : PuzzleBaseSpec(5934)
+class Day06LanternfishSpec : PuzzleBaseSpec(5934, 26984457539)
