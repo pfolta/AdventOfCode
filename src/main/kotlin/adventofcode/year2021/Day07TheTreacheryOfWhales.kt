56453b8021748de2b300d3ce7e483cadba44dc7b
Peter Folta <mail@peterfolta.net>
2021-12-07 07:10:19 +0000

    [2021/7] The Treachery of Whales (Part 1)

---
+++ src/main/kotlin/adventofcode/year2021/Day07TheTreacheryOfWhales.kt
@@ -0,0 +1,14 @@
+package adventofcode.year2021
+
+import adventofcode.Puzzle
+import kotlin.math.abs
+
+class Day07TheTreacheryOfWhales(customInput: String? = null) : Puzzle(customInput) {
+    override val name = "The Treachery of Whales"
+
+    private val crabs by lazy { input.split(",").map(String::toInt) }
+
+    override fun partOne() = generateSequence(crabs.minOrNull()!!) { it + 1 }
+        .take(crabs.maxOrNull()!! - crabs.minOrNull()!! + 1)
+        .minOf { crabs.sumOf { crab -> abs(it - crab) } }
+}
---
+++ src/test/kotlin/adventofcode/year2021/Day07TheTreacheryOfWhalesSpec.kt
@@ -0,0 +1,5 @@
+package adventofcode.year2021
+
+import adventofcode.PuzzleBaseSpec
+
+class Day07TheTreacheryOfWhalesSpec : PuzzleBaseSpec(37)
