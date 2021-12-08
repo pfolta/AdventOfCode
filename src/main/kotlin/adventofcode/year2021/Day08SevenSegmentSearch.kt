8f0f2aeab3cac1277d19e1fea0fe50c749604ab7
Peter Folta <mail@peterfolta.net>
2021-12-08 15:15:29 +0000

    [2021/8] Use pattern map for all digits

--- src/main/kotlin/adventofcode/year2021/Day08SevenSegmentSearch.kt
+++ src/main/kotlin/adventofcode/year2021/Day08SevenSegmentSearch.kt
@@ -19,13 +19,19 @@ class Day08SevenSegmentSearch(customInput: String? = null) : Puzzle(customInput)
         setOf('a', 'b', 'c', 'd', 'f', 'g') to 9
     )
 
-    private fun List<String>.getUniqueDigit(digit: Int) = when (digit) {
-        1 -> first { it.length == 2 }
-        4 -> first { it.length == 4 }
-        7 -> first { it.length == 3 }
-        8 -> first { it.length == 7 }
+    private fun List<String>.getPatternsForDigit(digit: Int) = when (digit) {
+        0 -> filter { it.length == 6 }
+        1 -> filter { it.length == 2 }
+        2 -> filter { it.length == 5 }
+        3 -> filter { it.length == 5 }
+        4 -> filter { it.length == 4 }
+        5 -> filter { it.length == 5 }
+        6 -> filter { it.length == 6 }
+        7 -> filter { it.length == 3 }
+        8 -> filter { it.length == 7 }
+        9 -> filter { it.length == 6 }
         else -> throw IllegalArgumentException("$digit is not a unique digit")
-    }.toSet()
+    }.map(String::toSet)
 
     override fun partOne() = outputValues.map { entry -> entry.filter { it.length in listOf(2, 3, 4, 7) } }.sumOf(List<String>::size)
 
@@ -34,42 +40,46 @@ class Day08SevenSegmentSearch(customInput: String? = null) : Puzzle(customInput)
             val signalPattern = signalPatterns[index]
 
             val a = signalPattern
-                .getUniqueDigit(7)
-                .minus(signalPattern.getUniqueDigit(1))
+                .getPatternsForDigit(7)
+                .first()
+                .minus(signalPattern.getPatternsForDigit(1).first())
                 .first()
 
             val g = signalPattern
-                .filter { it.length == 6 }
-                .map { it.toSet().minus(signalPattern.getUniqueDigit(4)).minus(signalPattern.getUniqueDigit(7)) }
+                .getPatternsForDigit(9)
+                .map { it.toSet().minus(signalPattern.getPatternsForDigit(4).first()).minus(signalPattern.getPatternsForDigit(7).first()) }
                 .first { it.size == 1 }
                 .first()
 
             val e = signalPattern
-                .getUniqueDigit(8)
+                .getPatternsForDigit(8)
+                .first()
                 .minus(setOf(a, g))
-                .minus(signalPattern.getUniqueDigit(4))
+                .minus(signalPattern.getPatternsForDigit(4).first())
                 .first()
 
             val b = signalPattern
-                .filter { it.length == 6 }
-                .map { it.toSet().minus(setOf(a, e, g)).minus(signalPattern.getUniqueDigit(1)) }
+                .getPatternsForDigit(6)
+                .map { it.toSet().minus(setOf(a, e, g)).minus(signalPattern.getPatternsForDigit(1).first()) }
                 .first { it.size == 1 }
                 .first()
 
             val d = signalPattern
-                .getUniqueDigit(8)
+                .getPatternsForDigit(8)
+                .first()
                 .minus(setOf(a, b, e, g))
-                .minus(signalPattern.getUniqueDigit(1))
+                .minus(signalPattern.getPatternsForDigit(1).first())
                 .first()
 
             val f = signalPattern
-                .filter { it.length == 6 }
+                .getPatternsForDigit(0)
                 .map { it.toSet().minus(setOf(a, b, d, e, g)) }
                 .first { it.size == 1 }
                 .first()
 
             val c = signalPattern
-                .getUniqueDigit(8)
+                .getPatternsForDigit(8)
+                .first()
                 .minus(setOf(a, b, d, e, f, g))
                 .first()
 
