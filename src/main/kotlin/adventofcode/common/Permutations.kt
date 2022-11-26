41c539807a40a1df6469bfe0083991eac70dbc3e
Peter Folta <mail@peterfolta.net>
2022-11-26 11:40:36 +0000

    Use `when` instead of `if` in Permutations util

--- src/main/kotlin/adventofcode/common/Permutations.kt
+++ src/main/kotlin/adventofcode/common/Permutations.kt
@@ -1,14 +1,16 @@
 package adventofcode.common
 
-fun <T : Any> Collection<T>.permutations(): List<List<T>> =
-    if (size <= 1) listOf(toList())
-    else drop(1).permutations().flatMap { permutation ->
+fun <T : Any> Collection<T>.permutations(): List<List<T>> = when {
+    size <= 1 -> listOf(toList())
+    else -> drop(1).permutations().flatMap { permutation ->
         (0..permutation.size).map { index -> permutation.subList(0, index) + first() + permutation.subList(index, permutation.size) }
     }
+}
 
-fun <T : Any> Collection<T>.powersets(): List<List<T>> =
-    if (isEmpty()) listOf(emptyList())
-    else {
-        val powersetOfRest = drop(1).powersets()
-        powersetOfRest + powersetOfRest.map { listOf(first()) + it }
+fun <T : Any> Collection<T>.powersets(): List<List<T>> = when {
+    isEmpty() -> listOf(emptyList())
+    else -> {
+        val powerSetOfRest = drop(1).powersets()
+        powerSetOfRest + powerSetOfRest.map { listOf(first()) + it }
     }
+}
