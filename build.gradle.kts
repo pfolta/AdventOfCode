94b3c7befdbb7b2773ba4a8f6eb33e5fbae8b09d
Peter Folta <mail@peterfolta.net>
2022-11-26 19:55:26 +0000

    Upgrade Kotest v4.6.3 -> v5.5.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
     implementation("org.reflections:reflections:0.9.12")
 
-    testImplementation("io.kotest:kotest-runner-junit5:4.6.3")
-    testImplementation("io.kotest:kotest-assertions-core:4.6.3")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
+    testImplementation("io.kotest:kotest-assertions-core:5.5.4")
 }
 
 tasks {
