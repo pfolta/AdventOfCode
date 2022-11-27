00a4881b2229f7ddcef06b5d3ce7011decb155c7
Peter Folta <mail@peterfolta.net>
2022-11-27 12:14:58 +0000

    Refactor `main` method

--- src/main/kotlin/adventofcode/Main.kt
+++ src/main/kotlin/adventofcode/Main.kt
@@ -7,7 +7,7 @@ fun main(vararg args: String) = args
     .map { it.split("/").map(String::toInt) }
     .flatMap { if (it.size == 2) listOf(Puzzles.forDay(it.first(), it.last())) else Puzzles.forYear(it.first()) }
     .ifEmpty { Puzzles.all() }
-    .forEach {
-        it.run()
+    .forEach { puzzle ->
+        puzzle.run()
         println()
     }
