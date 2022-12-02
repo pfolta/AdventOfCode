d0979d97e5fc9f4695fd1f02a4cd3dae547eb6b5
Peter Folta <mail@peterfolta.net>
2022-12-02 08:53:00 +0000

    [2022/1] Calorie Counting (Part 2)

--- src/main/kotlin/adventofcode/year2022/Day01CalorieCounting.kt
+++ src/main/kotlin/adventofcode/year2022/Day01CalorieCounting.kt
@@ -6,4 +6,6 @@ class Day01CalorieCounting(customInput: String? = null) : Puzzle(customInput) {
     private val elfCalories by lazy { input.split("\n\n").map { elf -> elf.lines().map(String::toInt) } }
 
     override fun partOne() = elfCalories.maxOf(List<Int>::sum)
+
+    override fun partTwo() = elfCalories.map(List<Int>::sum).sortedDescending().take(3).sum()
 }
--- src/test/kotlin/adventofcode/year2022/Day01CalorieCountingSpec.kt
+++ src/test/kotlin/adventofcode/year2022/Day01CalorieCountingSpec.kt
@@ -2,4 +2,4 @@ package adventofcode.year2022
 
 import adventofcode.PuzzleBaseSpec
 
-class Day01CalorieCountingSpec : PuzzleBaseSpec(24000)
+class Day01CalorieCountingSpec : PuzzleBaseSpec(24000, 45000)
