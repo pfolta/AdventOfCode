bbeaf42940f75e610c67aa8df48bf2459f2183d0
Peter Folta <mail@peterfolta.net>
2022-12-03 10:24:37 +0000

    [2022/3] Rucksack Reorganization (Part 2)

--- src/main/kotlin/adventofcode/year2022/Day03RucksackReorganization.kt
+++ src/main/kotlin/adventofcode/year2022/Day03RucksackReorganization.kt
@@ -6,13 +6,21 @@ class Day03RucksackReorganization(customInput: String? = null) : Puzzle(customIn
     private val rucksacks by lazy { input.lines() }
 
     override fun partOne() = rucksacks
-        .map { rucksack -> rucksack.substring(0 until rucksack.length / 2) to rucksack.substring(rucksack.length / 2) }
-        .map { rucksack -> rucksack.first.toSet() to rucksack.second.toSet() }
+        .map { rucksack -> rucksack.substring(0 until rucksack.length / 2).toSet() to rucksack.substring(rucksack.length / 2).toSet() }
         .map { rucksack -> rucksack.first.intersect(rucksack.second).first().toChar() }
-        .sumOf { item ->
-            when (item) {
-                in 'a'..'z' -> item.code - 96
-                else -> item.code - 38
-            }
+        .sumOf { item -> item.toPriority() }
+
+    override fun partTwo() = rucksacks
+        .chunked(3)
+        .map { group -> group.map(String::toSet) }
+        .flatMap { group -> group[0].intersect(group[1]).intersect(group[2]) }
+        .sumOf { item -> item.toPriority() }
+
+    companion object {
+        fun Char.toPriority() = when (this) {
+            in 'a'..'z' -> code - 96
+            in 'A'..'Z' -> code - 38
+            else -> throw IllegalArgumentException("'$this' is not a valid item type")
         }
+    }
 }
