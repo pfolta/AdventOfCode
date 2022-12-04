0e7e70bc638de6a14ae0a5d352fa1e8d28e1cec9
Peter Folta <mail@peterfolta.net>
2022-12-04 08:50:27 +0000

    [2022/4] Camp Cleanup (Part 2)

--- src/main/kotlin/adventofcode/year2022/Day04CampCleanup.kt
+++ src/main/kotlin/adventofcode/year2022/Day04CampCleanup.kt
@@ -6,19 +6,24 @@ class Day04CampCleanup(customInput: String? = null) : Puzzle(customInput) {
     private val assignmentPairs by lazy {
         input
             .lines()
-            .map { assignmentPair ->
-                ASSIGNMENT_PAIR_REGEX.find(assignmentPair)?.destructured
-                    ?: throw IllegalArgumentException("Invalid assignment pair '$assignmentPair'")
-            }.map { (a, b, c, d) -> a.toInt()..b.toInt() to c.toInt()..d.toInt() }
+            .map { ASSIGNMENT_PAIR_REGEX.find(it)?.destructured ?: throw IllegalArgumentException("Invalid assignment pair '$it'") }
+            .map { (a, b, c, d) -> a.toInt()..b.toInt() to c.toInt()..d.toInt() }
     }
 
     override fun partOne() = assignmentPairs
-        .filter { (first, second) -> first.contains(second) || second.contains(first) }
+        .filter { (first, second) -> first.fullyContains(second) || second.fullyContains(first) }
+        .size
+
+    override fun partTwo() = assignmentPairs
+        .filter { (first, second) -> first.overlapsWith(second) }
         .size
 
     companion object {
         private val ASSIGNMENT_PAIR_REGEX = """(\d+)-(\d+),(\d+)-(\d+)""".toRegex()
 
-        private fun IntRange.contains(other: IntRange) = contains(other.first) && contains(other.last)
+        private fun IntRange.fullyContains(other: IntRange) = contains(other.first) && contains(other.last)
+
+        private fun IntRange.overlapsWith(other: IntRange) =
+            contains(other.first) || contains(other.last) || other.contains(first) || other.contains(last)
     }
 }
--- src/test/kotlin/adventofcode/year2022/Day04CampCleanupSpec.kt
+++ src/test/kotlin/adventofcode/year2022/Day04CampCleanupSpec.kt
@@ -2,4 +2,4 @@ package adventofcode.year2022
 
 import adventofcode.PuzzleBaseSpec
 
-class Day04CampCleanupSpec : PuzzleBaseSpec(2)
+class Day04CampCleanupSpec : PuzzleBaseSpec(2, 4)
