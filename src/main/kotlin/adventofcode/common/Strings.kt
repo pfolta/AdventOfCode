5068231c9e7724cea5b3f447df66f493ba0e016e
Peter Folta <mail@peterfolta.net>
2022-12-05 23:41:31 +0000

    Use `BigInteger` to speed up generation of MD5 hashes

--- src/main/kotlin/adventofcode/common/Strings.kt
+++ src/main/kotlin/adventofcode/common/Strings.kt
@@ -1,5 +1,6 @@
 package adventofcode.common
 
+import java.math.BigInteger
 import java.security.MessageDigest
 
 /**
@@ -13,4 +14,4 @@ fun String.replaceAt(index: Int, replacement: Char) = substring(0 until index) +
 fun String.removeAt(index: Int, slice: Int = 1) = removeRange(index until index + slice)
 
 
-fun String.md5() = MessageDigest.getInstance("MD5").digest(toByteArray()).joinToString("") { "%02x".format(it) }
+fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16).padStart(32, '0')
