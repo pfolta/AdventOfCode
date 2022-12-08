7fa769334ce25cddf17756d9ffc241ec6b39958f
Peter Folta <mail@peterfolta.net>
2022-12-08 09:57:24 +0000

    [2022/8] Treetop Tree House (Part 2)

--- src/main/kotlin/adventofcode/year2022/Day08TreetopTreeHouse.kt
+++ src/main/kotlin/adventofcode/year2022/Day08TreetopTreeHouse.kt
@@ -1,6 +1,7 @@
 package adventofcode.year2022
 
 import adventofcode.Puzzle
+import adventofcode.common.product
 import adventofcode.year2022.Day08TreetopTreeHouse.Companion.Direction.DOWN
 import adventofcode.year2022.Day08TreetopTreeHouse.Companion.Direction.LEFT
 import adventofcode.year2022.Day08TreetopTreeHouse.Companion.Direction.RIGHT
@@ -12,25 +13,43 @@ class Day08TreetopTreeHouse(customInput: String? = null) : Puzzle(customInput) {
     override fun partOne() = treeMap
         .mapIndexed { y, row ->
             row.mapIndexed { x, tree ->
-                val tallerUpNeighbors = treeMap.neighbors(x, y, UP).filter { neighbor -> neighbor >= tree }
-                val tallerRightNeighbors = treeMap.neighbors(x, y, RIGHT).filter { neighbor -> neighbor >= tree }
-                val tallerDownNeighbors = treeMap.neighbors(x, y, DOWN).filter { neighbor -> neighbor >= tree }
-                val tallerLeftNeighbors = treeMap.neighbors(x, y, LEFT).filter { neighbor -> neighbor >= tree }
-
-                setOf(tallerUpNeighbors, tallerRightNeighbors, tallerDownNeighbors, tallerLeftNeighbors).any(List<Int>::isEmpty)
+                Direction
+                    .values()
+                    .map { direction -> treeMap.neighbors(x, y, direction).filter { neighbor -> neighbor >= tree } }
+                    .any(List<Int>::isEmpty)
             }
         }
         .flatten()
         .count { tree -> tree }
 
+    override fun partTwo() = treeMap
+        .mapIndexed { y, row ->
+            row.mapIndexed { x, tree ->
+                Direction
+                    .values()
+                    .map { direction ->
+                        val neighbors = treeMap.neighbors(x, y, direction)
+
+                        val visibleNeighbors = when (val tallestNeighbor = neighbors.indexOfFirst { neighbor -> neighbor >= tree }) {
+                            -1 -> neighbors
+                            else -> neighbors.take(tallestNeighbor + 1)
+                        }
+
+                        visibleNeighbors.size
+                    }
+            }
+        }
+        .flatten()
+        .maxOf(List<Int>::product)
+
     companion object {
         private enum class Direction { UP, RIGHT, DOWN, LEFT }
 
         private fun List<List<Int>>.neighbors(x: Int, y: Int, direction: Direction) = when (direction) {
-            UP -> map { row -> row[x] }.take(y)
+            UP -> map { row -> row[x] }.take(y).reversed()
             RIGHT -> get(y).drop(x + 1)
             DOWN -> map { row -> row[x] }.drop(y + 1)
-            LEFT -> get(y).take(x)
+            LEFT -> get(y).take(x).reversed()
         }
     }
 }
--- src/test/kotlin/adventofcode/year2022/Day08TreetopTreeHouseSpec.kt
+++ src/test/kotlin/adventofcode/year2022/Day08TreetopTreeHouseSpec.kt
@@ -2,4 +2,4 @@ package adventofcode.year2022
 
 import adventofcode.PuzzleBaseSpec
 
-class Day08TreetopTreeHouseSpec : PuzzleBaseSpec(21)
+class Day08TreetopTreeHouseSpec : PuzzleBaseSpec(21, 8)
