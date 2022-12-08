5c83b69e7e082ef095bb7a9735d6c0e69d7eab4f
Peter Folta <mail@peterfolta.net>
2022-12-08 11:26:54 +0000

    [2022/8] Simplify Part 2

--- src/main/kotlin/adventofcode/year2022/Day08TreetopTreeHouse.kt
+++ src/main/kotlin/adventofcode/year2022/Day08TreetopTreeHouse.kt
@@ -30,12 +30,10 @@ class Day08TreetopTreeHouse(customInput: String? = null) : Puzzle(customInput) {
                     .map { direction ->
                         val neighbors = treeMap.neighbors(x, y, direction)
 
-                        val visibleNeighbors = when (val tallestNeighbor = neighbors.indexOfFirst { neighbor -> neighbor >= tree }) {
-                            -1 -> neighbors
-                            else -> neighbors.take(tallestNeighbor + 1)
+                        when (val tallestNeighbor = neighbors.indexOfFirst { neighbor -> neighbor >= tree }) {
+                            -1 -> neighbors.size
+                            else -> tallestNeighbor + 1
                         }
-
-                        visibleNeighbors.size
                     }
             }
         }
@@ -43,13 +41,13 @@ class Day08TreetopTreeHouse(customInput: String? = null) : Puzzle(customInput) {
         .maxOf(List<Int>::product)
 
     companion object {
-        private enum class Direction { UP, RIGHT, DOWN, LEFT }
+        private enum class Direction { DOWN, LEFT, RIGHT, UP }
 
         private fun List<List<Int>>.neighbors(x: Int, y: Int, direction: Direction) = when (direction) {
-            UP -> map { row -> row[x] }.take(y).reversed()
-            RIGHT -> get(y).drop(x + 1)
             DOWN -> map { row -> row[x] }.drop(y + 1)
-            LEFT -> get(y).take(x).reversed()
+            LEFT -> this[y].take(x).reversed()
+            RIGHT -> this[y].drop(x + 1)
+            UP -> map { row -> row[x] }.take(y).reversed()
         }
     }
 }
