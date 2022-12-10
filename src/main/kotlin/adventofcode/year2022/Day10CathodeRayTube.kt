b52e0d617f53a92150bfc408bf8c6733ac10dd80
Peter Folta <mail@peterfolta.net>
2022-12-10 11:39:54 +0000

    [2022/10] Cathode-Ray Tube (Part 2)

--- src/main/kotlin/adventofcode/year2022/Day10CathodeRayTube.kt
+++ src/main/kotlin/adventofcode/year2022/Day10CathodeRayTube.kt
@@ -5,21 +5,42 @@ import adventofcode.Puzzle
 class Day10CathodeRayTube(customInput: String? = null) : Puzzle(customInput) {
     override val name = "Cathode-Ray Tube"
 
-    override fun partOne() = input
-        .lines()
-        .fold(listOf(Triple("", 0, 1))) { acc, instruction ->
-            val (_, _, xAfterPreviousCycle) = acc.last()
+    private val instructionCycles by lazy { input.toInstructionCycles() }
 
-            when {
-                instruction.startsWith("addx ") -> acc + listOf(
-                    Triple("noop", xAfterPreviousCycle, xAfterPreviousCycle),
-                    Triple(instruction, xAfterPreviousCycle, xAfterPreviousCycle + instruction.split(" ").last().toInt())
-                )
-
-                else -> acc + listOf(Triple(instruction, xAfterPreviousCycle, xAfterPreviousCycle))
-            }
-        }
-        .mapIndexed { cycle, (instruction, xBeforeCycle, _) -> Triple(cycle, instruction, xBeforeCycle) }
+    override fun partOne() = instructionCycles
         .filter { (cycle, _, _) -> cycle in setOf(20, 60, 100, 140, 180, 220) }
         .sumOf { (cycle, _, x) -> cycle * x }
+
+    override fun partTwo() = instructionCycles
+        .map { (cycle, _, x) ->
+            val pixel = (cycle - 1) % SCREEN_WIDTH
+            val spritePosition = listOf(x - 1, x, x + 1)
+
+            when {
+                spritePosition.contains(pixel) -> '█'
+                else -> ' '
+            }
+        }
+        .chunked(SCREEN_WIDTH)
+        .joinToString(separator = "\n", prefix = "\n", postfix = "\n") { row -> row.joinToString("") }
+
+    companion object {
+        private const val SCREEN_WIDTH = 40
+
+        private fun String.toInstructionCycles() = lines()
+            .fold(listOf(Triple("", 0, 1))) { acc, instruction ->
+                val (_, _, xAfterPreviousCycle) = acc.last()
+
+                acc + when {
+                    instruction.startsWith("addx ") -> listOf(
+                        Triple("noop", xAfterPreviousCycle, xAfterPreviousCycle),
+                        Triple(instruction, xAfterPreviousCycle, xAfterPreviousCycle + instruction.split(" ").last().toInt())
+                    )
+
+                    else -> listOf(Triple(instruction, xAfterPreviousCycle, xAfterPreviousCycle))
+                }
+            }
+            .mapIndexed { cycle, (instruction, xBeforeCycle, _) -> Triple(cycle, instruction, xBeforeCycle) }
+            .drop(1)
+    }
 }
--- src/test/kotlin/adventofcode/year2022/Day10CathodeRayTubeSpec.kt
+++ src/test/kotlin/adventofcode/year2022/Day10CathodeRayTubeSpec.kt
@@ -2,4 +2,16 @@ package adventofcode.year2022
 
 import adventofcode.PuzzleBaseSpec
 
-class Day10CathodeRayTubeSpec : PuzzleBaseSpec(13140)
+class Day10CathodeRayTubeSpec : PuzzleBaseSpec(
+    13140,
+    """
+        
+        ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  
+        ███   ███   ███   ███   ███   ███   ███ 
+        ████    ████    ████    ████    ████    
+        █████     █████     █████     █████     
+        ██████      ██████      ██████      ████
+        ███████       ███████       ███████     
+        
+    """.trimIndent()
+)
