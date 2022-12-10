17f23cc971c3505d8a4e0f3b7050f2584327a06a
Peter Folta <mail@peterfolta.net>
2022-12-10 17:18:55 +0000

    [2016/5] How About a Nice Game of Chess? (Part 2)

--- src/main/kotlin/adventofcode/year2016/Day05HowAboutANiceGameOfChess.kt
+++ src/main/kotlin/adventofcode/year2016/Day05HowAboutANiceGameOfChess.kt
@@ -14,6 +14,29 @@ class Day05HowAboutANiceGameOfChess(customInput: String? = null) : Puzzle(custom
             else -> index + 1 to password
         }
     }
-        .first { (_, password) -> password.length == 8 }
+        .first { (_, password) -> password.length == PASSWORD_LENGTH }
         .second
+
+    override fun partTwo() = generateSequence(0 to emptyMap<Int, Char>()) { (index, password) ->
+        val hash = "$input$index".md5()
+        val prefix = hash.substring(0, 5)
+        val position = hash[5].digitToIntOrNull()
+        val character = hash[6]
+
+        when {
+            prefix != "00000" -> index + 1 to password
+            position !in 0 until PASSWORD_LENGTH -> index + 1 to password
+            password.containsKey(position) -> index + 1 to password
+            else -> index + 1 to password + mapOf(position!! to character)
+        }
+    }
+        .first { (_, password) -> password.size == PASSWORD_LENGTH }
+        .second
+        .toSortedMap()
+        .values
+        .joinToString("")
+
+    companion object {
+        private const val PASSWORD_LENGTH = 8
+    }
 }
--- src/test/kotlin/adventofcode/year2016/Day05HowAboutANiceGameOfChessSpec.kt
+++ src/test/kotlin/adventofcode/year2016/Day05HowAboutANiceGameOfChessSpec.kt
@@ -2,4 +2,4 @@ package adventofcode.year2016
 
 import adventofcode.PuzzleBaseSpec
 
-class Day05HowAboutANiceGameOfChessSpec : PuzzleBaseSpec("18f47a30")
+class Day05HowAboutANiceGameOfChessSpec : PuzzleBaseSpec("18f47a30", "05ace8e3")
