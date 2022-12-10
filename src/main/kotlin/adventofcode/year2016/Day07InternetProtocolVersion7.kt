e60b24ddc90a59e48026e0825554fa1565c85ecd
Peter Folta <mail@peterfolta.net>
2022-12-10 15:00:17 +0000

    [2016/7] Internet Protocol Version 7 (Part 2)

--- src/main/kotlin/adventofcode/year2016/Day07InternetProtocolVersion7.kt
+++ src/main/kotlin/adventofcode/year2016/Day07InternetProtocolVersion7.kt
@@ -1,18 +1,27 @@
 package adventofcode.year2016
 
 import adventofcode.Puzzle
+import adventofcode.common.everyNth
 
 class Day07InternetProtocolVersion7(customInput: String? = null) : Puzzle(customInput) {
     private val ipv7Addresses by lazy {
-        input.lines().map { line -> line to HYPERNET_REGEX.findAll(line).map(MatchResult::value).toList() }
+        input.lines().map { ipAddress -> ipAddress.split("[", "]") }.map { parts -> parts.everyNth(2) to parts.everyNth(2, 1) }
     }
 
     override fun partOne() = ipv7Addresses
-        .count { (ipAddress, hypernets) -> ipAddress.containsAbba() && hypernets.none { hypernet -> hypernet.containsAbba() } }
+        .count { (supernets, hypernets) ->
+            supernets.any { supernet -> supernet.containsAbba() } && hypernets.none { hypernet -> hypernet.containsAbba() }
+        }
+
+    override fun partTwo() = ipv7Addresses
+        .count { (supernets, hypernets) ->
+            val abas = supernets.flatMap { supernet -> supernet.getAbas() }
+            hypernets.any { hypernet -> abas.any { (a, b) -> hypernet.contains("$b$a$b") } }
+        }
 
     companion object {
-        private val HYPERNET_REGEX = """\[(\w*)]""".toRegex()
+        private fun String.containsAbba() = windowed(4).map(String::toList).any { (a, b, c, d) -> (a != b && "$a$b" == "$d$c") }
 
-        private fun String.containsAbba() = windowed(4).map(String::toCharArray).any { (a, b, c, d) -> (a != b && "$a$b" == "$d$c") }
+        private fun String.getAbas() = windowed(3).map(String::toList).filter { (a, b, c) -> (a != b && a == c) }
     }
 }
