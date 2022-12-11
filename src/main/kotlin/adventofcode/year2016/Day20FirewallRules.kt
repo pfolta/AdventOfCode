2b2cdaebbf093f0b2da5ed853af11d491f8bc3c2
Peter Folta <mail@peterfolta.net>
2022-12-11 14:29:54 +0000

    [2016/20] Firewall Rules (Part 2)

--- src/main/kotlin/adventofcode/year2016/Day20FirewallRules.kt
+++ src/main/kotlin/adventofcode/year2016/Day20FirewallRules.kt
@@ -1,19 +1,25 @@
 package adventofcode.year2016
 
 import adventofcode.Puzzle
+import kotlin.math.max
 
 class Day20FirewallRules(customInput: String? = null) : Puzzle(customInput) {
     private val rules by lazy {
         input
             .lines()
-            .map { RULE_REGEX.find(it)?.destructured ?: throw IllegalArgumentException("'$it' is not a valid firewall rule") }
+            .map { rule -> rule.split("-") }
             .map { (from, to) -> from.toUInt()..to.toUInt() }
-            .sortedBy { it.first }
+            .sortedBy { rule -> rule.first }
+            .fold(emptyList<UIntRange>()) { rules, rule ->
+                when {
+                    rules.isEmpty() -> listOf(rule)
+                    rule.first - 1U <= rules.last().last -> rules.dropLast(1) + listOf(rules.last().first..max(rules.last().last, rule.last))
+                    else -> rules + listOf(rule)
+                }
+            }
     }
 
-    override fun partOne() = generateSequence(0.toUInt(), UInt::inc).first { ip -> rules.none { rule -> rule.contains(ip) } }
+    override fun partOne() = rules.first().last + 1U
 
-    companion object {
-        private val RULE_REGEX = """(\d+)-(\d+)""".toRegex()
-    }
+    override fun partTwo() = UInt.MAX_VALUE - rules.sumOf { rule -> rule.last - rule.first + 1U } + 1U
 }
--- src/test/kotlin/adventofcode/year2016/Day20FirewallRulesSpec.kt
+++ src/test/kotlin/adventofcode/year2016/Day20FirewallRulesSpec.kt
@@ -2,4 +2,4 @@ package adventofcode.year2016
 
 import adventofcode.PuzzleBaseSpec
 
-class Day20FirewallRulesSpec : PuzzleBaseSpec(3U)
+class Day20FirewallRulesSpec : PuzzleBaseSpec(3U, 4294967288U)
