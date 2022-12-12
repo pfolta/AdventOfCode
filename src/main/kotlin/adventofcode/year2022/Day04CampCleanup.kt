cbdbe98078a18f1a998587309ced8cf5d0147fa2
Peter Folta <mail@peterfolta.net>
2022-12-12 21:14:17 +0000

    [2022/4] Use custom extension functions for `contains` and `overlaps`

--- src/main/kotlin/adventofcode/year2022/Day04CampCleanup.kt
+++ src/main/kotlin/adventofcode/year2022/Day04CampCleanup.kt
@@ -6,19 +6,19 @@ class Day04CampCleanup(customInput: String? = null) : Puzzle(customInput) {
     private val assignmentPairs by lazy {
         input
             .lines()
-            .map { ASSIGNMENT_PAIR_REGEX.find(it)?.destructured ?: throw IllegalArgumentException("'$it' is not a valid assignment pair") }
+            .map { ASSIGNMENT_PAIR_REGEX.find(it)!!.destructured }
             .map { (a, b, c, d) -> a.toInt()..b.toInt() to c.toInt()..d.toInt() }
     }
 
-    override fun partOne() = assignmentPairs
-        .filter { (first, second) -> first.intersect(second).size in setOf(first.count(), second.count()) }
-        .size
+    override fun partOne() = assignmentPairs.count { (a, b) -> a in b || b in a }
 
-    override fun partTwo() = assignmentPairs
-        .filter { (first, second) -> first.intersect(second).isNotEmpty() }
-        .size
+    override fun partTwo() = assignmentPairs.count { (a, b) -> a overlaps b }
 
     companion object {
         private val ASSIGNMENT_PAIR_REGEX = """(\d+)-(\d+),(\d+)-(\d+)""".toRegex()
+
+        private operator fun IntRange.contains(other: IntRange) = first <= other.first && last >= other.last
+
+        private infix fun IntRange.overlaps(other: IntRange) = first <= other.last && other.first <= last
     }
 }
