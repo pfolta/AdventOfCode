3b5282c51c282dbbd968a6d3d2cc58a85114291d
Peter Folta <mail@peterfolta.net>
2022-12-29 00:09:58 +0000

    [2016/12] Leonardo's Monorail (Part 2)

--- src/main/kotlin/adventofcode/year2016/Day12LeonardosMonorail.kt
+++ src/main/kotlin/adventofcode/year2016/Day12LeonardosMonorail.kt
@@ -4,16 +4,17 @@ import adventofcode.Puzzle
 import adventofcode.PuzzleInput
 
 private typealias InstructionIndex = Int
-private typealias Registers = MutableMap<Char, Int>
+private typealias Registers = MutableMap<String, Int>
 
 class Day12LeonardosMonorail(customInput: PuzzleInput? = null) : Puzzle(customInput) {
     override val name = "Leonardo's Monorail"
 
-    override fun partOne() = input.lines().runAssembunny()['a']!!
+    override fun partOne() = input.lines().runAssembunny()["a"]!!
+
+    override fun partTwo() = input.lines().runAssembunny(mutableMapOf("c" to 1))["a"]!!
 
     companion object {
-        private fun List<String>.runAssembunny(): Registers {
-            val registers = mutableMapOf('a' to 0, 'b' to 0, 'c' to 0, 'd' to 0)
+        private fun List<String>.runAssembunny(registers: Registers = mutableMapOf()): Registers {
             var index = 0
 
             while (index < this.size) {
@@ -29,34 +30,35 @@ class Day12LeonardosMonorail(customInput: PuzzleInput? = null) : Puzzle(customIn
             return when (instruction.first()) {
                 "cpy" -> {
                     val source = instruction[1]
-                    val destination = instruction.last().first()
+                    val destination = instruction.last()
 
                     when {
                         source.toIntOrNull() != null -> registers[destination] = source.toInt()
-                        else -> registers[destination] = registers[source.first()]!!
+                        else -> registers[destination] = registers.getOrDefault(source, 0)
                     }
 
                     index + 1
                 }
 
                 "inc" -> {
-                    val register = instruction.last().first()
-                    registers[register] = registers[register]!! + 1
+                    val register = instruction.last()
+                    registers[register] = registers.getOrDefault(register, 0) + 1
                     index + 1
                 }
 
                 "dec" -> {
-                    val register = instruction.last().first()
-                    registers[register] = registers[register]!! - 1
+                    val register = instruction.last()
+                    registers[register] = registers.getOrDefault(register, 0) - 1
                     index + 1
                 }
 
                 "jnz" -> {
-                    val register = instruction[1].first()
+                    val test = instruction[1]
                     val offset = instruction.last().toInt()
 
-                    when (registers[register]) {
-                        0 -> index + 1
+                    when {
+                        test.toIntOrNull() == 0 -> index + 1
+                        test.toIntOrNull() == null && registers.getOrDefault(test, 0) == 0 -> index + 1
                         else -> index + offset
                     }
                 }
