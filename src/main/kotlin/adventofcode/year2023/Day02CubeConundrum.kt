b3505a7b83a7d79dc77299cce2cfab92b2a03453
Peter Folta <mail@peterfolta.net>
2023-12-02 09:28:38 +0000

    [2023/2] Cube Conundrum (Part 1)

---
+++ src/main/kotlin/adventofcode/year2023/Day02CubeConundrum.kt
@@ -0,0 +1,48 @@
+package adventofcode.year2023
+
+import adventofcode.Puzzle
+import adventofcode.PuzzleInput
+
+class Day02CubeConundrum(customInput: PuzzleInput? = null) : Puzzle(customInput) {
+    override fun partOne() = input
+        .lines()
+        .map(Game::invoke)
+        .filter { game -> game.isPossible(12, 13, 14) }
+        .sumOf(Game::id)
+
+    companion object {
+        private data class Turn(
+            val red: Int,
+            val green: Int,
+            val blue: Int
+        ) {
+            companion object {
+                operator fun invoke(turn: String): Turn {
+                    val red = """(\d+) red""".toRegex().find(turn)?.destructured?.component1()?.toInt() ?: 0
+                    val green = """(\d+) green""".toRegex().find(turn)?.destructured?.component1()?.toInt() ?: 0
+                    val blue = """(\d+) blue""".toRegex().find(turn)?.destructured?.component1()?.toInt() ?: 0
+
+                    return Turn(red, green, blue)
+                }
+            }
+        }
+
+        private data class Game(
+            val id: Int,
+            val turns: List<Turn>
+        ) {
+            fun isPossible(red: Int, green: Int, blue: Int) = turns
+                .filterNot { turn -> turn.red <= red && turn.green <= green && turn.blue <= blue }
+                .isEmpty()
+
+            companion object {
+                operator fun invoke(input: String): Game {
+                    val id = input.split(": ").first().split(" ").last().toInt()
+                    val turns = input.split(": ").last().split("; ").map(Turn::invoke)
+
+                    return Game(id, turns)
+                }
+            }
+        }
+    }
+}
