aefce80fec6a0a188125f265e7744e33ed50b298
Peter Folta <mail@peterfolta.net>
2024-12-08 11:35:37 +0000

    [2024/8] Resonant Collinearity (Part 2)

--- src/main/kotlin/adventofcode/year2024/Day08ResonantCollinearity.kt
+++ src/main/kotlin/adventofcode/year2024/Day08ResonantCollinearity.kt
@@ -10,26 +10,38 @@ class Day08ResonantCollinearity(customInput: PuzzleInput? = null) : Puzzle(custo
 
     override fun partOne() =
         input
-            .lines()
-            .flatMapIndexed { y, row -> row.mapIndexed { x, char -> (x to y) to char } }
-            .filterNot { (_, char) -> char == '.' }
-            .groupBy({ it.second }, { it.first })
-            .map { (char, antennas) ->
-                char to antennas.flatMap { a -> antennas.mapNotNull { b -> if (a != b) setOf(a, b) else null } }.toSet()
+            .distinctAntinodes { (a, b) ->
+                val distance = b - a
+                setOf(a - distance, b + distance).filter { it.isInBounds((gridSize)) }.toSet()
             }
-            .map { (char, antennaPairs) ->
-                char to
-                    antennaPairs.map { pair ->
-                        val distance = pair.last() - pair.first()
-                        setOf(pair.first() - distance, pair.last() + distance).filter { it.isInBounds((gridSize)) }
-                    }
+
+    override fun partTwo() =
+        input
+            .distinctAntinodes { (a, b) ->
+                val distance = b - a
+                val antinodes = mutableSetOf(a)
+                var candidate = a - distance
+
+                while (candidate.isInBounds(gridSize)) {
+                    antinodes += candidate
+                    candidate -= distance
+                }
+
+                antinodes
             }
-            .map { (_, antinodes) -> antinodes.flatten() }
-            .reduce { allAntinodes, antinodes -> allAntinodes + antinodes }
-            .toSet()
-            .size
 
     companion object {
-        fun Pair<Int, Int>.isInBounds(gridSize: Int) = toList().all { it in 0 until gridSize }
+        private fun Pair<Int, Int>.isInBounds(gridSize: Int) = toList().all { it in 0 until gridSize }
+
+        private fun String.distinctAntinodes(antinodeFunction: (Pair<Pair<Int, Int>, Pair<Int, Int>>) -> Set<Pair<Int, Int>>) =
+            lines()
+                .flatMapIndexed { y, row -> row.mapIndexed { x, char -> (x to y) to char } }
+                .filterNot { (_, char) -> char == '.' }
+                .groupBy({ it.second }, { it.first })
+                .map { (_, antennas) -> antennas.flatMap { a -> antennas.mapNotNull { b -> if (a != b) a to b else null } }.toSet() }
+                .map { antennaPairs -> antennaPairs.flatMap { pair -> antinodeFunction(pair) } }
+                .reduce { allAntinodes, antinodes -> allAntinodes + antinodes }
+                .toSet()
+                .size
     }
 }
