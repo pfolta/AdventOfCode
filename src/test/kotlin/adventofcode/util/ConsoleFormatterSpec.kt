7584b09195d2821cbd8f15b71f89ad5050e4bb50
Peter Folta <mail@peterfolta.net>
2024-12-08 19:06:18 +0000

    Add additional tests for `ConsoleFormatter`

--- src/test/kotlin/adventofcode/util/ConsoleFormatterSpec.kt
+++ src/test/kotlin/adventofcode/util/ConsoleFormatterSpec.kt
@@ -2,6 +2,7 @@ package adventofcode.util
 
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
+import java.net.URI
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
@@ -17,6 +18,18 @@ class ConsoleFormatterSpec : FreeSpec({
 
         "highlights Durations of more than 15s in red" {
             1.minutes.formatBenchmark() shouldBe "\u001B[41;37;1m1m\u001B[0m"
+        }
+    }
+
+    "String.bold()" - {
+        "uses the correct ANSI control characters to format text as bold" {
+            "Example Text".bold() shouldBe "\u001B[1mExample Text\u001B[0m"
+        }
+    }
+
+    "URI.formatUri()" - {
+        "uses the correct ANSI control characters to render a URI in blue and underlined" {
+            URI("https://adventofcode.com").formatUri() shouldBe "\u001B[34;4mhttps://adventofcode.com\u001B[0m"
         }
     }
 })
