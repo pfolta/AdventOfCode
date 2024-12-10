520bfcb48398773900e6449ba9adcecb29af0ae3
Peter Folta <mail@peterfolta.net>
2024-12-10 20:05:47 +0000

    [2024/10] Use the same BFS for both parts

--- src/main/kotlin/adventofcode/year2024/Day10HoofIt.kt
+++ src/main/kotlin/adventofcode/year2024/Day10HoofIt.kt
@@ -9,27 +9,19 @@ class Day10HoofIt(customInput: PuzzleInput? = null) : Puzzle(customInput) {
         input.lines().mapIndexed { y, line -> line.mapIndexed { x, height -> x to y to (height.digitToIntOrNull() ?: -1) } }
     }
 
-    override fun partOne() =
-        map
-            .flatten()
-            .filter { (_, height) -> height == 0 }
-            .sumOf { (trailhead, _) -> map.countTrails(trailhead, 9, true, 1) }
+    private val trailheads by lazy { map.flatten().filter { (_, height) -> height == 0 } }
 
-    override fun partTwo() =
-        map
-            .flatten()
-            .filter { (_, height) -> height == 9 }
-            .sumOf { (trailhead, _) -> map.countTrails(trailhead, 0, false, -1) }
+    override fun partOne() = trailheads.sumOf { (trailhead, _) -> map.countTrails(trailhead, true) }
+
+    override fun partTwo() = trailheads.sumOf { (trailhead, _) -> map.countTrails(trailhead, false) }
 
     companion object {
         private fun List<List<Pair<Pair<Int, Int>, Int>>>.countTrails(
-            start: Pair<Int, Int>,
-            end: Int,
+            trailhead: Pair<Int, Int>,
             distinct: Boolean,
-            delta: Int,
         ): Int {
             val visited = mutableListOf<Pair<Int, Int>>()
-            val queue = ArrayDeque(setOf(start))
+            val queue = ArrayDeque(setOf(trailhead))
 
             while (queue.isNotEmpty()) {
                 val position = queue.removeFirst()
@@ -40,14 +32,14 @@ class Day10HoofIt(customInput: PuzzleInput? = null) : Puzzle(customInput) {
 
                     val nextPositions =
                         neighbors(x, y, false)
-                            .filter { (a, b) -> this[b][a].second - this[y][x].second == delta }
+                            .filter { (a, b) -> this[b][a].second == this[y][x].second + 1 }
                             .filterNot { it in visited }
 
                     queue.addAll(nextPositions)
                 }
             }
 
-            return visited.count { (x, y) -> this[y][x].second == end }
+            return visited.count { (x, y) -> this[y][x].second == 9 }
         }
     }
 }
