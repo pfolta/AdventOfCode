29f0325374feb22cabc066672568299d20e0a331
Peter Folta <mail@peterfolta.net>
2024-12-20 12:01:48 +0000

    [2024/17] Chronospatial Computer (Part 2)

--- src/main/kotlin/adventofcode/year2024/Day17ChronospatialComputer.kt
+++ src/main/kotlin/adventofcode/year2024/Day17ChronospatialComputer.kt
@@ -2,83 +2,96 @@ package adventofcode.year2024
 
 import adventofcode.Puzzle
 import adventofcode.PuzzleInput
-import kotlin.math.pow
 
 class Day17ChronospatialComputer(customInput: PuzzleInput? = null) : Puzzle(customInput) {
-    private val registers by lazy { input.lines().take(3).map { it.substringAfter(": ").toInt() } }
+    private val registers by lazy { input.lines().take(3).map { it.substringAfter(": ").toLong() } }
     private val program by lazy { input.lines().last().substringAfter(": ").split(",").map(String::toInt) }
 
-    override fun partOne(): String {
-        var (registerA, registerB, registerC) = registers
-        var pointer = 0
-        val output = mutableListOf<Int>()
+    override fun partOne() = runProgram(program, registers).joinToString(",")
 
-        while (pointer < program.size - 1) {
-            val opcode = program[pointer]
-            val literalOperand = program[pointer + 1]
-
-            val comboOperand =
-                when (literalOperand) {
-                    in 0..3 -> literalOperand
-                    4 -> registerA
-                    5 -> registerB
-                    6 -> registerC
-                    else -> throw IllegalArgumentException("$literalOperand is not a valid combo operand")
-                }
-
-            when (opcode) {
-                0 -> {
-                    registerA = adv(registerA, comboOperand)
-                    pointer += 2
-                }
-
-                1 -> {
-                    registerB = registerB.xor(literalOperand)
-                    pointer += 2
-                }
-
-                2 -> {
-                    registerB = comboOperand.mod(8)
-                    pointer += 2
-                }
-
-                3 -> {
-                    pointer =
-                        when (registerA) {
-                            0 -> pointer + 2
-                            else -> literalOperand
-                        }
-                }
-
-                4 -> {
-                    registerB = registerB.xor(registerC)
-                    pointer += 2
-                }
-
-                5 -> {
-                    output += comboOperand.mod(8)
-                    pointer += 2
-                }
-
-                6 -> {
-                    registerB = adv(registerA, comboOperand)
-                    pointer += 2
-                }
-
-                7 -> {
-                    registerC = adv(registerA, comboOperand)
-                    pointer += 2
+    override fun partTwo() =
+        program
+            .reversed()
+            .map(Int::toLong)
+            .fold(listOf(0L)) { candidates, instruction ->
+                candidates.flatMap { candidate ->
+                    val shifted = candidate shl 3
+                    (shifted..shifted + 8).mapNotNull { registerA ->
+                        registerA.takeIf { runProgram(program, listOf(registerA) + registers.drop(1)).first() == instruction }
+                    }
                 }
             }
-        }
-
-        return output.joinToString(",")
-    }
+            .first()
 
     companion object {
-        private fun adv(
-            numerator: Int,
-            comboOperand: Int,
-        ) = (numerator / 2.0.pow(comboOperand)).toInt()
+        private fun runProgram(
+            program: List<Int>,
+            registers: List<Long>,
+        ): List<Long> {
+            var (registerA, registerB, registerC) = registers
+            var pointer = 0
+            val output = mutableListOf<Long>()
+
+            while (pointer < program.size - 1) {
+                val opcode = program[pointer]
+                val literalOperand = program[pointer + 1]
+
+                val comboOperand =
+                    when (literalOperand) {
+                        in 0..3 -> literalOperand.toLong()
+                        4 -> registerA
+                        5 -> registerB
+                        6 -> registerC
+                        else -> throw IllegalArgumentException("$literalOperand is not a valid combo operand")
+                    }
+
+                when (opcode) {
+                    0 -> {
+                        registerA = registerA shr comboOperand.toInt()
+                        pointer += 2
+                    }
+
+                    1 -> {
+                        registerB = registerB xor literalOperand.toLong()
+                        pointer += 2
+                    }
+
+                    2 -> {
+                        registerB = comboOperand.mod(8L)
+                        pointer += 2
+                    }
+
+                    3 -> {
+                        pointer =
+                            when (registerA) {
+                                0L -> pointer + 2
+                                else -> literalOperand
+                            }
+                    }
+
+                    4 -> {
+                        registerB = registerB xor registerC
+                        pointer += 2
+                    }
+
+                    5 -> {
+                        output += comboOperand.mod(8).toLong()
+                        pointer += 2
+                    }
+
+                    6 -> {
+                        registerB = registerA shr comboOperand.toInt()
+                        pointer += 2
+                    }
+
+                    7 -> {
+                        registerC = registerA shr comboOperand.toInt()
+                        pointer += 2
+                    }
+                }
+            }
+
+            return output
+        }
     }
 }
--- src/test/kotlin/adventofcode/year2024/Day17ChronospatialComputerSpec.kt
+++ src/test/kotlin/adventofcode/year2024/Day17ChronospatialComputerSpec.kt
@@ -2,4 +2,23 @@ package adventofcode.year2024
 
 import adventofcode.PuzzleBaseSpec
 
-class Day17ChronospatialComputerSpec : PuzzleBaseSpec("4,6,3,5,6,3,5,2,1,0")
+class Day17ChronospatialComputerSpec : PuzzleBaseSpec(
+    listOf(
+        """
+        Register A: 729
+        Register B: 0
+        Register C: 0
+
+        Program: 0,1,5,4,3,0
+        """.trimIndent() to "4,6,3,5,6,3,5,2,1,0",
+    ),
+    listOf(
+        """
+        Register A: 2024
+        Register B: 0
+        Register C: 0
+
+        Program: 0,3,5,4,3,0
+        """.trimIndent() to 117440,
+    ),
+)
