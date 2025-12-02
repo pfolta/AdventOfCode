f5f5c38f8376ac2665b5a93a6cddb3dc3e8dfc9c
Peter Folta <mail@peterfolta.net>
2025-12-02 16:10:12 +0000

    [Gradle] Upgrade Kotlin v2.1.0 -> v2.2.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     application
     jacoco
 
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "2.2.21"
 
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
     id("com.adarshr.test-logger") version "4.0.0"
