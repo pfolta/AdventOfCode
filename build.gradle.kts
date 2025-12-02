150ace0d5c68edc7de894e887381efd20fbdad95
Peter Folta <mail@peterfolta.net>
2025-12-02 16:24:11 +0000

    [Gradle] Upgrade SLF4J NOP Provider v2.0.16 -> v2.0.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation("com.diogonunes:JColor:5.5.1")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.1")
     implementation("org.reflections:reflections:0.10.2")
-    implementation("org.slf4j:slf4j-nop:2.0.16")
+    implementation("org.slf4j:slf4j-nop:2.0.17")
 
     testImplementation("io.kotest:kotest-runner-junit5-jvm:5.9.1")
     testImplementation("io.kotest:kotest-assertions-core-jvm:5.9.1")
