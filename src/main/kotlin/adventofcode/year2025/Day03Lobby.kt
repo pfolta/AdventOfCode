befb65c7e4f02034886679545130ba0edf356fd5
Peter Folta <mail@peterfolta.net>
2025-12-04 01:34:19 +0000

    [2025/3] Use `IndexedValue` and regular `fold` instead of `runningFold`

--- src/main/kotlin/adventofcode/year2025/Day03Lobby.kt
+++ src/main/kotlin/adventofcode/year2025/Day03Lobby.kt
@@ -6,18 +6,17 @@ import adventofcode.PuzzleInput
 class Day03Lobby(
     customInput: PuzzleInput? = null,
 ) : Puzzle(customInput) {
-    private fun parseInput(): List<List<Int>> = input.lines().map { bank -> bank.toList().map(Character::getNumericValue) }
+    private fun parseInput(): List<List<IndexedValue<Int>>> = input.lines().map { bank -> bank.map(Char::digitToInt).withIndex().toList() }
 
-    private fun List<List<Int>>.totalOutputJoltage(batteryCount: Int) =
+    private fun List<List<IndexedValue<Int>>>.totalOutputJoltage(batteryCount: Int) =
         sumOf { bank ->
             (batteryCount downTo 1)
-                .runningFold(Triple<Int, Int, Int?>(0, bank.size - batteryCount, null)) { (lIndex, rIndex), battery ->
-                    val slice = bank.subList(lIndex, rIndex + 1)
-                    val max = slice.max()
-                    Triple((slice.indexOf(max) + lIndex + 1), bank.size - battery + 1, max)
-                }.mapNotNull { it.third }
-                .joinToString("")
-                .toLong()
+                .fold(Triple(0, bank.size - batteryCount, 0L)) { (lIndex, rIndex, joltage), battery ->
+                    bank
+                        .subList(lIndex, rIndex + 1)
+                        .maxBy { battery -> battery.value }
+                        .let { max -> Triple(max.index + 1, bank.size - battery + 1, joltage * 10 + max.value) }
+                }.third
         }
 
     override fun partOne() = parseInput().totalOutputJoltage(2)
