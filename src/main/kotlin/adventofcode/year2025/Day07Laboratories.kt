b7f4e885036bdc836c78d7217bfe72c66c0c0341
Peter Folta <mail@peterfolta.net>
2025-12-08 00:03:18 +0000

    [2025/7] Laboratories (Part 2)

--- src/main/kotlin/adventofcode/year2025/Day07Laboratories.kt
+++ src/main/kotlin/adventofcode/year2025/Day07Laboratories.kt
@@ -7,11 +7,9 @@ import adventofcode.common.spatial.Grid2d
 class Day07Laboratories(
     customInput: PuzzleInput? = null,
 ) : Puzzle(customInput) {
-    private fun parseInput() = Grid2d(input)
-
-    override fun partOne(): Int {
-        val grid = parseInput()
-        val start = grid[START].x.toInt()
+    private fun parseInput(): Pair<Long, List<Set<Long>>> {
+        val grid = Grid2d(input)
+        val start = grid[START].x
         val splittersByRow =
             grid
                 .rows()
@@ -19,17 +17,44 @@ class Day07Laboratories(
                     row
                         .mapIndexedNotNull { x, char ->
                             when (char) {
-                                SPLITTER -> x
+                                SPLITTER -> x.toLong()
                                 else -> null
                             }
                         }.toSet()
                 }
+
+        return Pair(start, splittersByRow)
+    }
+
+    override fun partOne(): Int {
+        val (start, splittersByRow) = parseInput()
 
         return splittersByRow
             .fold(Pair(setOf(start), 0)) { (beam, splitCount), splittersInRow ->
                 val reachedSplitters = beam intersect splittersInRow
                 Pair(beam - reachedSplitters + reachedSplitters.flatMap { x -> setOf(x - 1, x + 1) }, splitCount + reachedSplitters.size)
             }.second
+    }
+
+    override fun partTwo(): Long {
+        val (start, splittersByRow) = parseInput()
+        val beam = mutableMapOf(start to 1L)
+
+        for (splittersInRow in splittersByRow) {
+            val reachedSplitters = splittersInRow intersect beam.keys
+
+            for (reachedSplitter in reachedSplitters) {
+                val pathCount = beam[reachedSplitter] ?: 0
+
+                if (pathCount > 0) {
+                    beam[reachedSplitter - 1] = (beam[reachedSplitter - 1] ?: 0) + pathCount
+                    beam[reachedSplitter + 1] = (beam[reachedSplitter + 1] ?: 0) + pathCount
+                    beam[reachedSplitter] = 0
+                }
+            }
+        }
+
+        return beam.values.sum()
     }
 
     companion object {
--- src/test/kotlin/adventofcode/year2025/Day07LaboratoriesSpec.kt
+++ src/test/kotlin/adventofcode/year2025/Day07LaboratoriesSpec.kt
@@ -2,4 +2,4 @@ package adventofcode.year2025
 
 import adventofcode.PuzzleBaseSpec
 
-class Day07LaboratoriesSpec : PuzzleBaseSpec(21)
+class Day07LaboratoriesSpec : PuzzleBaseSpec(21, 40)
