9d38e3e64484b52eb5a5223a3d27285ab90aa95b
Peter Folta <mail@peterfolta.net>
2025-12-08 12:08:07 +0000

    [2025/8] Playground (Part 2)

--- src/main/kotlin/adventofcode/year2025/Day08Playground.kt
+++ src/main/kotlin/adventofcode/year2025/Day08Playground.kt
@@ -9,33 +9,52 @@ import adventofcode.util.runningInTest
 class Day08Playground(
     customInput: PuzzleInput? = null,
 ) : Puzzle(customInput) {
-    private fun parseInput() =
-        input.lines().map {
-            val (x, y, z) = junctionBoxRegex.find(it)!!.destructured
-            Point3d(x.toLong(), y.toLong(), z.toLong())
-        }
+    private fun connectJunctionBoxes(): Sequence<Triple<Set<Set<Point3d>>, Int, Set<Point3d>>> {
+        val junctionBoxes =
+            input.lines().map {
+                val (x, y, z) = junctionBoxRegex.find(it)!!.destructured
+                Point3d(x.toLong(), y.toLong(), z.toLong())
+            }
 
-    override fun partOne(): Int {
-        val junctionBoxes = parseInput()
         val sortedJunctionBoxPairs =
             junctionBoxes
                 .flatMapIndexed { index, a -> junctionBoxes.subList(index + 1, junctionBoxes.size).map { b -> a to b } }
                 .map { (a, b) -> Triple(a, b, a euclideanDistanceTo b) }
                 .sortedBy { (_, _, distance) -> distance }
 
+        return generateSequence(Triple(junctionBoxes.map { box -> setOf(box) }.toSet(), 0, emptySet())) { (circuits, index) ->
+            val (a, b) = sortedJunctionBoxPairs[index]
+            val circuitsContainingAorB = circuits.filter { circuit -> circuit.contains(a) || circuit.contains(b) }
+
+            Triple(
+                circuits - circuitsContainingAorB.toSet() + setOf(circuitsContainingAorB.flatten().toSet() + setOf(a) + setOf(b)),
+                index + 1,
+                setOf(a, b),
+            )
+        }
+    }
+
+    override fun partOne(): Int {
         // The example case shows making 10 connections, the actual puzzle requires 1000 connections
         val numberOfConnections = if (runningInTest()) 10 else 1000
 
-        return (0 until numberOfConnections)
-            .fold(junctionBoxes.map { box -> setOf(box) }.toSet()) { circuits, index ->
-                val (a, b) = sortedJunctionBoxPairs[index]
-                val circuitsContainingAorB = circuits.filter { circuit -> circuit.contains(a) || circuit.contains(b) }
-                circuits - circuitsContainingAorB.toSet() + setOf(circuitsContainingAorB.flatten().toSet() + setOf(a) + setOf(b))
-            }.map { circuit -> circuit.size }
+        return connectJunctionBoxes()
+            .drop(1)
+            .take(numberOfConnections)
+            .last()
+            .first
+            .map { circuit -> circuit.size }
             .sorted()
             .takeLast(3)
             .product()
     }
+
+    override fun partTwo(): Long =
+        connectJunctionBoxes()
+            .first { (circuits) -> circuits.size == 1 }
+            .third
+            .map { (x) -> x }
+            .product()
 
     companion object {
         private val junctionBoxRegex = """(\d+),(\d+),(\d+)""".toRegex()
--- src/test/kotlin/adventofcode/year2025/Day08PlaygroundSpec.kt
+++ src/test/kotlin/adventofcode/year2025/Day08PlaygroundSpec.kt
@@ -2,4 +2,4 @@ package adventofcode.year2025
 
 import adventofcode.PuzzleBaseSpec
 
-class Day08PlaygroundSpec : PuzzleBaseSpec(40)
+class Day08PlaygroundSpec : PuzzleBaseSpec(40, 25272)
