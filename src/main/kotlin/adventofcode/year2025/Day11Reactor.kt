9c8f222efc20a71a566e9af77f5aa9605f14e7ab
Peter Folta <mail@peterfolta.net>
2025-12-11 12:56:47 +0000

    [2025/11] Reactor (Part 2)

--- src/main/kotlin/adventofcode/year2025/Day11Reactor.kt
+++ src/main/kotlin/adventofcode/year2025/Day11Reactor.kt
@@ -3,6 +3,8 @@ package adventofcode.year2025
 import adventofcode.Puzzle
 import adventofcode.PuzzleInput
 
+private typealias Device = String
+
 class Day11Reactor(
     customInput: PuzzleInput? = null,
 ) : Puzzle(customInput) {
@@ -12,16 +14,28 @@ class Day11Reactor(
             .map { line -> line.split(": ", limit = 2) }
             .associate { (fromDevice, toDevices) -> Pair(fromDevice, toDevices.split(" ").toSet()) }
 
-    override fun partOne() = parseInput().countPaths(START)
+    override fun partOne() = parseInput().countPaths(YOU)
+
+    override fun partTwo() = parseInput().countPaths(SVR, mustVisit = setOf(DAC, FFT))
 
     companion object {
-        private const val START = "you"
+        private const val DAC = "dac"
         private const val END = "out"
+        private const val FFT = "fft"
+        private const val SVR = "svr"
+        private const val YOU = "you"
 
-        private fun Map<String, Set<String>>.countPaths(device: String): Long =
-            when (device) {
-                END -> 1
-                else -> this.getValue(device).sumOf { next -> countPaths(next) }
+        private fun Map<Device, Set<Device>>.countPaths(
+            device: Device,
+            pathCounts: HashMap<Pair<Device, Set<Device>>, Long> = hashMapOf(),
+            mustVisit: Set<Device> = emptySet(),
+        ): Long =
+            pathCounts.getOrPut(Pair(device, mustVisit)) {
+                when (device) {
+                    END if mustVisit.isEmpty() -> 1
+                    END -> 0
+                    else -> getValue(device).sumOf { next -> countPaths(next, pathCounts, mustVisit - device) }
+                }
             }
     }
 }
--- src/test/kotlin/adventofcode/year2025/Day11ReactorSpec.kt
+++ src/test/kotlin/adventofcode/year2025/Day11ReactorSpec.kt
@@ -2,4 +2,37 @@ package adventofcode.year2025
 
 import adventofcode.PuzzleBaseSpec
 
-class Day11ReactorSpec : PuzzleBaseSpec(5)
+class Day11ReactorSpec :
+    PuzzleBaseSpec(
+        listOf(
+            """
+            aaa: you hhh
+            you: bbb ccc
+            bbb: ddd eee
+            ccc: ddd eee fff
+            ddd: ggg
+            eee: out
+            fff: out
+            ggg: out
+            hhh: ccc fff iii
+            iii: out
+            """.trimIndent() to 5,
+        ),
+        listOf(
+            """
+            svr: aaa bbb
+            aaa: fft
+            fft: ccc
+            bbb: tty
+            tty: ccc
+            ccc: ddd eee
+            ddd: hub
+            hub: fff
+            eee: dac
+            dac: fff
+            fff: ggg hhh
+            ggg: out
+            hhh: out
+            """.trimIndent() to 2,
+        ),
+    )
